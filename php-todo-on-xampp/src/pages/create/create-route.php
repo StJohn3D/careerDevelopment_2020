963e94628bbd55378e02ebbf603f7f1c4d85b62a
StJohn3D <stjohn3d@outlook.com>
2020-06-20 23:01:03 -0400

    Refactored create-route to use Page and AuthedUser components

--- php-todo-on-xampp/src/pages/create/create-route.php
+++ php-todo-on-xampp/src/pages/create/create-route.php
@@ -1,42 +1,37 @@
 <?php
   require_once('create-ctrl.php');
+  require_once('./components/Page.php');
+  require_once('./components/AuthedHeader.php');
+
+  $headerContent = AuthedHeader::render($userData);
+
+  $titleErrorMessage = $titleState->valid ? "" : <<<XML
+    <b>$titleState->errorMessage</b>
+  XML;
+
+  $descriptionErrorMessage = $descriptionState->valid ? "" : <<<XML
+    <b>$descriptionState->errorMessage</b>
+  XML;
+
+  $bodyContent = <<<XML
+    <form method="post" action="create.php">
+      <div class="form-control">
+        <label for="title">Title $titleErrorMessage</label>
+        <input type="text" name="title" id="title"
+          required aria-required="true" minlength="4" maxlength="55"
+          value="$title"
+        />
+      </div>
+      <div class="form-control">
+        <label for="description">Description $descriptionErrorMessage</label>
+        <textarea name="description" id="description"
+          aria-required="false" maxlength="256"
+        />$description</textarea>
+      </div>
+      <a href="/todoapp/index.php?">Cancel</a>
+      <input type="submit" name="submit" value="Create"/>
+    </form>
+  XML;
+
+  new Page("Create new ToDo list", $headerContent, $bodyContent);
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>ToDo App</title>
-</head>
-<body>
-  <main>
-    <h1>ToDo App</h1>
-    <section>
-      <h1>Create new ToDo list</h1>
-      <form method="post" action="create.php">
-        <div class="form-control">
-          <label for="title">Title<?php
-            if(!$titleState->valid) { echo ": <b>$titleState->errorMessage</b>"; }
-          ?></label>
-          <input type="text" name="title" id="title"
-            required aria-required="true" minlength="4" maxlength="55"
-            value="<?php echo $title; ?>"
-          />
-        </div>
-        <div class="form-control">
-          <label for="description">Description<?php
-            if(!$descriptionState->valid) { echo ": <b>$descriptionState->errorMessage</b>"; }
-          ?></label>
-          <textarea name="description" id="description"
-            aria-required="false" maxlength="256"
-          />
-            <?php echo $description; ?>
-          </textarea>
-        </div>
-        <a href="/todoapp/index.php?">Cancel</a>
-        <input type="submit" name="submit" value="Create"/>
-      </form>
-    </section>
-  </main>
-</body>
-</html>
