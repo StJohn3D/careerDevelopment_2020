bc70a9da387bf6b0348433aa1abf813b46eb32b4
StJohn3D <stjohn3d@outlook.com>
2020-06-28 13:33:07 -0400

    Improving WebHint and LightHouse scores

--- php-todo-on-xampp/src/components/Page.php
+++ php-todo-on-xampp/src/components/Page.php
@@ -1,4 +1,7 @@
 <?php
+// use https://www.srihash.org/ to generate integrity hashes
+
+header_remove("Server"); //This isn't working and neither does the 'Header unset Server' directive in the .htaccess file
 
 class Page {
   public function __construct($title, $headerContent = "", $bodyContent = "", $footerContent = "", $style = "") {
@@ -10,10 +13,13 @@ class Page {
       <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Create all the ToDo lists you need! Have one for groceries, shopping etc.. Easily add ToDo items and check them off when you're done!">
         <title>ToDo App - $title</title>
         <link rel="stylesheet" href="/todoapp/styles/theme.css">
         $stylesheet
         <link
+          integrity="sha384-QyOIuAdNFUvnfP6FGfqShjmsibA4Sbvg0dd5LqjC40PJXlPjHjuGkDO3ySgKz95K"
+          crossorigin="anonymous"
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap"
           rel="stylesheet"
         >
