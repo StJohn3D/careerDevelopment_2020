c6b90044fefcb2eeaba04ed728a665aafc8e7830
StJohn3D <stjohn3d@outlook.com>
2020-06-30 22:41:37 -0400

    List Details Edit styles

--- php-todo-on-xampp/src/pages/todo/TodoList_Details_Edit.php
+++ php-todo-on-xampp/src/pages/todo/TodoList_Details_Edit.php
@@ -1,10 +1,13 @@
 <?php
 require_once('./db/TodoList.php');
 require_once('./lib/fieldHelpers.php');
+require_once('./lib/attributeHelpers.php');
 require_once('./pages/create/create-formValidators.php');
 
 class TodoList_Details_Edit {
   public static function render($todoListData) {
+    require_once('./components/requiredAsterisk.php');
+
     $title = getFieldValue('todo_list_title', $todoListData->title);
     $description = getFieldValue('todo_list_description', $todoListData->description);
 
@@ -22,33 +25,42 @@ class TodoList_Details_Edit {
       header('Location: '.$uri."/todoapp/todo.php?id=$todoListData->id");
       exit;
     }
+    
+    if (!isset($_POST['submit'])) {
+      $titleState->valid = true;
+      $descriptionState->valid = true;
+    }
 
     $titleErrorMessage = $titleState->valid ? "" : <<<XML
       <b>$titleState->errorMessage</b>
     XML;
+    $titleInvalidAttributes = getInvalidAttributes($titleState->valid);
 
     $descriptionErrorMessage = $descriptionState->valid ? "" : <<<XML
       <b>$descriptionState->errorMessage</b>
     XML;
+    $descriptionInvalidAttributes = getInvalidAttributes($descriptionState->valid);
 
     return <<<XML
-      <section class="todo-list-details" style="border: 1px solid; padding: 16px;">
+      <section class="active-form">
         <form method="post">
           <div class="form-control">
-            <label for="todo_list_title">Title $titleErrorMessage</label>
+            <label for="todo_list_title">Title$requiredAsterisk $titleErrorMessage</label>
             <input type="text" name="todo_list_title" id="todo_list_title"
-              required aria-required="true" minlength="4" maxlength="256"
+              required aria-required="true" minlength="4" maxlength="55" $titleInvalidAttributes
               value="$title"
             />
           </div>
           <div class="form-control">
             <label for="todo_list_description">Description $descriptionErrorMessage</label>
             <textarea name="todo_list_description" id="todo_list_description"
-            aria-required="false" maxlength="256"
+            aria-required="false" rows="5" maxlength="256" $descriptionInvalidAttributes
             />$description</textarea>
           </div>
-          <a class="cancel-icon" href="/todoapp/todo.php?id=$todoListData->id">Cancel</a>
-          <input type="submit" name="submit" value="Save"/>
+          <div class="button-row">
+            <a class="btn" href="/todoapp/todo.php?id=$todoListData->id">Cancel</a>
+            <input class="btn btn--primary" type="submit" name="submit" value="Save"/>
+          </div>
         </form>
       </section>
     XML;
