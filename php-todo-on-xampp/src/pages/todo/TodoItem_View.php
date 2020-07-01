cd2bb3dbc91d8f52264df4142d54f8558e466839
StJohn3D <stjohn3d@outlook.com>
2020-07-01 00:55:58 -0400

    ToDo completed view styles + disabled edit

--- php-todo-on-xampp/src/pages/todo/TodoItem_View.php
+++ php-todo-on-xampp/src/pages/todo/TodoItem_View.php
@@ -34,21 +34,36 @@ class TodoItem_View {
       echo "<meta http-equiv='refresh' content='0'>";
     }
 
-    $editButton = "";
-    if (!$todoData->completed) {
-      $editButton = <<<XML
-        <a $disabledState $ariaDisabled class="btn" href="/todoapp/todo.php?id=$todoListId&edit=$todoData->id">Edit</a>
-      XML;
-    }
-    
-    return <<<XML
-      <article class="todo-item-card">
-        <div class="grid-area--title">
-          <h1 class="todo-item-card__title">$todoData->title</h1>
-        </div>
+    $description = "";
+    if ($todoData->description !== null && $todoData->description !== "") {
+      $description = <<<XML
         <div class="grid-area--description">
           <p class="todo-item-card__description">$todoData->description</p>
         </div>
+      XML;
+    }
+
+    $editButton = "";
+    if (!$todoData->completed) {
+      if ($editButtonsEnabled) {
+        $editButton = <<<XML
+          <a class="btn" href="/todoapp/todo.php?id=$todoListId&edit=$todoData->id">Edit</a>
+        XML;
+      } else {
+        $editButton = <<<XML
+          <button $disabledState $ariaDisabled class="btn" title="disabled">Edit</button>
+        XML;
+      }
+    }
+
+    $completedClass = $todoData->completed ? " todo-item-card--completed" : "";
+    
+    return <<<XML
+      <article class="todo-item-card$completedClass">
+        <div class="grid-area--title">
+          <h1 class="todo-item-card__title">$todoData->title</h1>
+        </div>
+        $description
         <div class="grid-area--due-date">
           $dueDate
         </div>
@@ -58,8 +73,8 @@ class TodoItem_View {
           </form>
         </div>
         <div class="grid-area--actions">
-          $editButton
           <form method="post">
+            $editButton
             <input type="submit" name="$deleteFieldName" id="$deleteFieldName" class="btn btn--danger" title="Delete todo item: $todoData->title" value="Delete"/>
           </form>
         </div>
