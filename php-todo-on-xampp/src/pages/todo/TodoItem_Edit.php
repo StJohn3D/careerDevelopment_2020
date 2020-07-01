f189f01e1201e2938ce61d601e972f9dcb7d2820
StJohn3D <stjohn3d@outlook.com>
2020-06-30 22:52:35 -0400

    TodoItem Edit styles

--- php-todo-on-xampp/src/pages/todo/TodoItem_Edit.php
+++ php-todo-on-xampp/src/pages/todo/TodoItem_Edit.php
@@ -1,10 +1,13 @@
 <?php
 require_once('./db/Todo.php');
 require_once('./lib/fieldHelpers.php');
+require_once('./lib/attributeHelpers.php');
 require_once('todo-formValidators.php');
 
 class TodoItem_Edit {
   public static function render($todoData, $todoListId) {
+    require_once('./components/requiredAsterisk.php');
+
     $title = getFieldValue('todo_title', $todoData->title);
     $description = getFieldValue('todo_description', $todoData->description);
 
@@ -37,43 +40,54 @@ class TodoItem_Edit {
       exit;
     }
 
+    if (!isset($_POST['submit'])) {
+      $titleState->valid = true;
+      $descriptionState->valid = true;
+      $dueDateState->valid = true;
+    }
+
     $titleErrorMessage = $titleState->valid ? "" : <<<XML
       <b>$titleState->errorMessage</b>
     XML;
+    $titleInvalidAttributes = getInvalidAttributes($titleState->valid);
 
     $descriptionErrorMessage = $descriptionState->valid ? "" : <<<XML
       <b>$descriptionState->errorMessage</b>
     XML;
+    $descriptionInvalidAttributes = getInvalidAttributes($descriptionState->valid);
 
     $dueDateErrorMessage = $dueDateState->valid ? "" : <<<XML
       <b>$dueDateState->errorMessage</b>
     XML;
+    $dueDateInvalidAttributes = getInvalidAttributes($dueDateState->valid);
 
     return <<<XML
-      <article className="todo_card" style="border: 1px solid; padding: 16px;">
+      <article class="active-form">
         <form method="post">
           <div class="form-control">
-            <label for="todo_title">Title $titleErrorMessage</label>
+            <label for="todo_title">Title$requiredAsterisk $titleErrorMessage</label>
             <input type="text" name="todo_title" id="todo_title"
-              required aria-required="true" minlength="4" maxlength="55"
+              required aria-required="true" minlength="4" maxlength="55" $titleInvalidAttributes
               value="$title"
             />
           </div>
           <div class="form-control">
             <label for="todo_description">Description $descriptionErrorMessage</label>
             <textarea name="todo_description" id="todo_description"
-              aria-required="false" maxlength="256"
+              aria-required="false" rows="5" maxlength="256" $descriptionInvalidAttributes
             />$description</textarea>
           </div>
           <div class="form-control">
             <label for="todo_due_date">Due Date $dueDateErrorMessage</label>
             <input type="date" name="todo_due_date" id="todo_due_date"
-              aria-required="false" maxlength="256"
+              aria-required="false" maxlength="256" $dueDateInvalidAttributes
               value="$dueDate"
             />
           </div>
-          <a class="cancel-icon" href="/todoapp/todo.php?id=$todoListId">Cancel</a>
-          <input type="submit" name="submit" value="Save"/>
+          <div class="button-row">
+            <a class="btn" href="/todoapp/todo.php?id=$todoListId">Cancel</a>
+            <input class="btn btn--primary" type="submit" name="submit" value="Save"/>
+          </div>
         </form>
       </article>
     XML;
